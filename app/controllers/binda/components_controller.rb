997047c9ca2e7283c7ad2e1eb84734d618af8a1f
tau9 <marco.panzeri@bitrock.it>
2022-06-16 19:29:37 +0200

    add: logs

--- app/controllers/binda/components_controller.rb
+++ app/controllers/binda/components_controller.rb
@@ -88,6 +88,7 @@ module Binda
         render json: upload_details, status: 200
       else
         print "here"
+        puts @component.inspect
         puts upload_params(:component).inspect
         puts @component.errors.full_messages.inspect
         render json: @component.errors.full_messages, status: 400
