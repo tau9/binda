c131e54cdc25f350fcaaac3369d90b930d9830c6
tau9 <marco.panzeri@bitrock.it>
2022-06-22 12:18:29 +0200

    add: logs

--- app/jobs/binda/generate_fields_job.rb
+++ app/jobs/binda/generate_fields_job.rb
@@ -4,9 +4,11 @@ module Binda
 
     def perform instance
       # If this is a component or a board
+	  puts instance.inspect
     	if instance.respond_to?('structure')
 	    	field_settings = FieldSetting.where(field_group_id: FieldGroup.where(structure_id: instance.structure.id))
 	    	field_settings.each do |field_setting|
+				puts field_setting
 	    		"Binda::#{field_setting.field_type.classify}".constantize.find_or_create_by!(
 	    			fieldable_id: instance.id, fieldable_type: instance.class.name, field_setting_id: field_setting.id )
 	    	end
